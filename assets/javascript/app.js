931e49efadc021452eb364f7b964f6887cb6caa3
Alistair Rowden <deadlyhardware@gmail.com>
2019-09-24 18:40:21 -0400

    Clean up Js, switched to camel case for variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,18 +22,18 @@ $(document).ready(function() {
     // =================================================
     // clear local storage unique id to test as new user
     // localStorage.clear();
-    // use existing unique user_key to pull data and test returning user experience...
-    // localStorage.setItem('CE_app_unique_user_id', '-LbR6BxBLel84VvGrCe_');
+    // use existing unique userKey to pull data and test returning user experience...
+    // localStorage.setItem('UniqueUserId', '-LbR6BxBLel84VvGrCe_');
     // =================================================
 
     // declare global variables
-    let user_key,
-        user_zip;
-    let user_interests = [];
+    let userKey,
+        userZip;
+    let userInterests = [];
     // if unique key is stored locally and exists... 
-    if (localStorage.getItem('CE_app_unique_user_id')) {
+    if (localStorage.getItem('UniqueUserId')) {
         // store it in a reusable variable...
-        var uid = localStorage.getItem('CE_app_unique_user_id');
+        var uid = localStorage.getItem('UniqueUserId');
         // test to make sure this variable works...
         console.log(uid);
         // access the database to check for a value/key...
@@ -54,34 +54,34 @@ $(document).ready(function() {
                 // test log the uid's data...
                 console.log(obj.val());
                 // place the uid's data in a variable...
-                var user_obj = obj.val();
+                var userObj = obj.val();
                 // another test log of the uid's data...
-                console.log(user_obj);
+                console.log(userObj);
                 // test log that we can retrieve the user's interests. Since it is an array, use .length to see if we can get how many interests there are...
-                console.log(user_obj['interests'].length);
+                console.log(userObj['interests'].length);
                 // test log that we can retieve the user's zip...
-                console.log(user_obj['zip']);
+                console.log(userObj['zip']);
                 // console.log('=======================');
-                // set the user_key to the uid...
-                user_key = uid;
+                // set the userKey to the uid...
+                userKey = uid;
                 // loop through the database's array of interests and store them into a global variable...
-                for (var i = 0; i < user_obj['interests'].length; i++) {
-                    // push each interest retrieved from the database into user_interests variable...
-                    user_interests.push(user_obj['interests'][i]);
+                for (var i = 0; i < userObj['interests'].length; i++) {
+                    // push each interest retrieved from the database into userInterests variable...
+                    userInterests.push(userObj['interests'][i]);
                 }
                 // test log that data grab was successful...
-                console.log(user_interests);
+                console.log(userInterests);
                 // grab the database's stored zip and store it globally...
-                user_zip = user_obj['zip'];
+                userZip = userObj['zip'];
                 // test log zip retrieval success...
-                console.log(user_zip);
+                console.log(userZip);
 
             });
             
             // call function to check the checkboxes the user is interested in
-            checkInterest(user_interests);
+            checkInterest(userInterests);
             // call function to load the user's last zip input
-            checkZip(user_zip);
+            checkZip(userZip);
         }
         } console.log('why doesn\'t this print?');
         
@@ -89,24 +89,24 @@ $(document).ready(function() {
         // test that it does not exist by logging it...
         console.log('local storage id does not exist');
         // store the new unique id provided by the database to local storage...
-        localStorage.setItem('CE_app_unique_user_id', database.ref().child('data').push().key);
-        // declare a global variable 'user_key' to be the same as the retrieved unique id retrieved from the database and stored to local storage...
-        user_key = localStorage.getItem('CE_app_unique_user_id');
+        localStorage.setItem('UniqueUserId', database.ref().child('data').push().key);
+        // declare a global variable 'userKey' to be the same as the retrieved unique id retrieved from the database and stored to local storage...
+        userKey = localStorage.getItem('UniqueUserId');
         // test that the ids are being stored correctly by logging both to the console...
-        console.log('Had to create a new local storage id. It is: '+localStorage.getItem('CE_app_unique_user_id'));
+        console.log('Had to create a new local storage id. It is: ' + localStorage.getItem('UniqueUserId'));
         // ...
-        console.log('new user key is: ' + user_key);
+        console.log('new user key is: ' + userKey);
     }
 
     // tests to see if retrieving user data is successfully global...
-    gatheredData(user_key, user_interests, user_zip);
+    gatheredData(userKey, userInterests, userZip);
     function gatheredData() {
         // wait for the data to be retrieved from database
         setTimeout(function() {
             console.log('===== GATHERED DATA =====');
-            console.log(user_key);
-            console.log(user_interests);
-            console.log(user_zip);
+            console.log(userKey);
+            console.log(userInterests);
+            console.log(userZip);
         }, 5000 ); // waits 5 seconds before test logging
     }
     // ===========================================================
@@ -131,39 +131,37 @@ $(document).ready(function() {
         "Fishing",
         "Running"
       ];
-    // test log the categories variable...
-    console.log(categories);
     // use a for loop to dynamically generate check box input fields for each category...
     for (var i = 0; i < categories.length; i++) {
         // generate half on one side...
         if (i < categories.length / 2) {
             // generate a bootstrap checkbox...
-            var form_1 = $(`
+            var form1 = $(`
             <div class="form-check">
                 <label class="form-check-label">
                 <input class="form-check-input" type="checkbox" value="${categories[i]}" name="interests[]">${categories[i]}</label>
             </div>
             `);
             // spawn the generated html to the page...
-            $('#interests_1').append(form_1);
+            $('#interests1').append(form1);
         } else { // ...and the other half on the next side...
             // generate a bootstrap checkbox...
-            var form_2 = $(`
+            var form2 = $(`
             <div class="form-check">
                 <label class="form-check-label">
                 <input class="form-check-input" type="checkbox" value="${categories[i]}" name="interests[]">${categories[i]}</label>
             </div>
             `);
             // spawn the generated html to the page...
-            $('#interests_2').append(form_2);
+            $('#interests2').append(form2);
         }
     }
     // ===========================================================
-    // dynamically check the check boxes from grabbed stored data of user_interests...
+    // dynamically check the check boxes from grabbed stored data of userInterests...
     function checkInterest(interested) {
         for (var i = 0; i < interested.length; i++) {
             // test log 
-            console.log('User has '+interested.length+' interests.');
+            console.log('User has ' + interested.length + ' interests.');
             var interest = interested[i];
             // for each interest, we want to target that interest's checkbox, and check it
             $(`input[value='${interest}']`).prop('checked', true);
@@ -171,15 +169,15 @@ $(document).ready(function() {
     }
     // dynamically fill in the zipcode from grabbed stored data of zip...
     function checkZip(zip) {
-        $('#zip_code_input').val(zip);
+        $('#zipCodeInput').val(zip);
     }
     // ===========================================================
     // obtain reference to checkboxes named 'interests[]'
     var interests = $('input[name="interests[]"]');
     // test 'interests' are being successfully retreived. Should print that it is an object (array)
-    console.log('here are the interests: '+ interests);
+    console.log('here are the interests: ' + interests);
     // capture all inputs when #check_events_button is clicked. Use event delegation since the check_events_button will be checking for elements that exist dynamically...
-    $(document).on('click', '#check_events_button', function(event) {
+    $(document).on('click', '#checkEventsButton', function(event) {
         event.preventDefault();
         triggerEvent();
     });
@@ -197,9 +195,9 @@ $(document).ready(function() {
 
     function triggerEvent() {
         // empty the array of interests to recapture the updated checkboxes...This will prevent from doubling the interests by adding do the array...
-        user_interests = [];
+        userInterests = [];
         // test logs that the array has been emptied. Should print 0...
-        console.log('Retrieved '+user_interests.length+' interests from user');
+        console.log('Retrieved ' + userInterests.length + ' interests from user');
         // prevent button from self click...
         event.preventDefault();
         // capture each user checked interest
@@ -210,40 +208,40 @@ $(document).ready(function() {
             if (interests[i].checked) {
                 // test log that it was checked...
                 console.log(interests[i].value + ' was checked');
-                // push the checked interest into the user_interests array...
-                user_interests.push(interests[i].value);
+                // push the checked interest into the userInterests array...
+                userInterests.push(interests[i].value);
             }
         }
-        // test user_interests has updated from emptied array to containing values...
-        console.log('User interests has updated to '+user_interests.length+' interests...');
+        // test userInterests has updated from emptied array to containing values...
+        console.log('User interests has updated to ' + userInterests.length + ' interests...');
 
         // get user's zip code input...
-        let user_zip = $('#zip_code_input').val().trim();
+        let userZip = $('#zipCodeInput').val().trim();
         // test log captured zip input...
-        console.log(user_zip, user_interests);
+        console.log(userZip, userInterests);
         // call a declared function that will store captured input into the database, passing in the user's input...
-        storeUserData(user_zip, user_interests);
+        storeUserData(userZip, userInterests);
 
-        // declare a function that will add the user's input to the database under their unique id/user_key...
+        // declare a function that will add the user's input to the database under their unique id/userKey...
         function storeUserData(zip, interests) {
             // store the passed data into an object for that will be stored under the user's uid in the database...
-            let user_data = {
+            let userData = {
                 // set a key relative to the data...
                 zip: zip,
                 interests: interests
             };
-            // test log that user_data was successfully generated...
-            console.log(user_data);
+            // test log that userData was successfully generated...
+            console.log(userData);
             
             // declare an empty object variable that will be used as a cargo to pass in updated data to the database...
             var updates = {};
             // create a database key/url pathway that will hold the users stored data...
-            updates['/data/' + user_key] = user_data;
+            updates['/data/' + userKey] = userData;
             // test log the cargo-object and it's content...
             console.log(updates);
             // inform user that their info will do stored. Could ask permission to store their info onto database by swapping the alert() function to a confirm() function...
             alert('We\'ll keep your interests in mind :)');
-            // uses firebases's update() function to update the unique database pathway with user_data...
+            // uses firebases's update() function to update the unique database pathway with userData...
             return database.ref().update(updates);
         }
         //---------------------
@@ -251,11 +249,11 @@ $(document).ready(function() {
         var results = [];
 
         // // make an ajax call for each interest
-        for (let i = 0; i < user_interests.length; i++) {
+        for (let i = 0; i < userInterests.length; i++) {
             
             $.ajax({
                 url:
-                  `https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search/?radius=50&zip=${user_zip}&current_page=1&per_page=12&sort=date_asc&topic=${user_interests[i]}&start_date=2019-04-06..&api_key=x428fd5p5e9xmjfqwngw9fag`,
+                  `https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search/?radius=50&zip=${userZip}&current_page=1&per_page=12&sort=date_asc&topic=${userInterests[i]}&start_date=2019-04-06..&api_key=x428fd5p5e9xmjfqwngw9fag`,
                 method: "GET"
               }).then(function(data) {
                 console.log(data);
@@ -285,7 +283,7 @@ $(document).ready(function() {
                 }
 
               }).then(function(){
-                console.log('AJAX call #'+i+' complete. Data was stored...');
+                console.log('AJAX call #' + i + ' complete. Data was stored...');
               }).then();
         } 
 
@@ -302,12 +300,12 @@ $(document).ready(function() {
         console.log('The results are: '+ results);
       
         // load event options dynamically...
-        var event_options = $('#event_options');
+        var eventOptions = $('#eventOptions');
         //======================================
         // needs to load after 7 milli-seconds...until more research is done for async/await, this is a solution...
         setTimeout(function(){
             if (results.length === 0) {
-                alert('Sorry, there isn\'t anything happening in your area at the moment :(');
+                alert('Sorry, there isn\'t anything happening in your area at the moment');
             }
             for (let i = 0; i < 12; i++) {
                 let name = results[i].name,
@@ -318,7 +316,6 @@ $(document).ready(function() {
                     addr = results[i].stAddr,
                     phone = results[i].phone,
                     org = results[i].org;
-                    // descr = descr.substr(0, descr.length-4);
                     date = date.substr(0, date.length-9);
                     lon = results[i].lon;
                     lat = results[i].lat;
@@ -346,7 +343,7 @@ $(document).ready(function() {
                     <iframe src="" frameborder="0">${renderMap}</iframe>
                     `);
     
-                event_options.append(`
+                eventOptions.append(`
                 <div class="col-md-5 p-3 border m-2">
                     <div class="row text-center text-light">
                         <div class="col-md-12">
